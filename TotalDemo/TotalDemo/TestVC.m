feeba1c828447bab8c90903c53008136815e93d9
wangzhaotao <tyler.wang>
2019-04-09 21:07:49 +0800

    函数调用栈

--- TotalDemo/TotalDemo/TestVC.m
+++ TotalDemo/TotalDemo/TestVC.m
@@ -16,17 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    //
+    
+    int array[10];
+    for (int i=0; i<10; i++) {
+        array[i] = i;
+    }
+    int *p = array;
+    int *q = &array[2];
+    
+    printf("q-p = %d \n", q-p);
+    printf("array[q-p] = %d \n", array[q-p]);
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
+
+
+
 
 @end
